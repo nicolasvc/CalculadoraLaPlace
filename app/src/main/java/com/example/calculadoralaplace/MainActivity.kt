61f36710a1fbd6458e6e290c7df1d14bc5bd4423
Nicolas <nicovc20@gmail.com>
2021-10-04 20:47:10 -0500

    Ajuste logica para que realize las operaciones basicas

--- app/src/main/java/com/example/calculadoralaplace/MainActivity.kt
+++ app/src/main/java/com/example/calculadoralaplace/MainActivity.kt
@@ -1,59 +1,174 @@
 package com.example.calculadoralaplace
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.calculadoralaplace.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.buttons_layout.*
 import kotlinx.android.synthetic.main.input_layout.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var canAddOperation = false
+    private var canAddDecimal = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        initView()
     }
 
-    private fun initView(){
-        btn0.setOnClickListener { setProcess("0")}
-        btn1.setOnClickListener { setProcess("1")}
-        btn2.setOnClickListener { setProcess("2")}
-        btn3.setOnClickListener { setProcess("3")}
-        btn4.setOnClickListener { setProcess("4")}
-        btn5.setOnClickListener { setProcess("5")}
-        btn6.setOnClickListener { setProcess("6")}
-        btn7.setOnClickListener { setProcess("7")}
-        btn8.setOnClickListener { setProcess("8")}
-        btn9.setOnClickListener { setProcess("9")}
-        btnSum.setOnClickListener { setProcess("+")}
-        btnRest.setOnClickListener { setProcess("-")}
-        btnmultiply.setOnClickListener { setProcess("x")}
-        btnDivision.setOnClickListener { setProcess("÷")}
-        btnPercent.setOnClickListener { setProcess("%")}
-        btnClear.setOnClickListener {cleanCalculator()}
-        btnEquals.setOnClickListener { operateFunction() }
+    fun numberAction(view: View)
+    {
+        if(view is Button)
+        {
+            if(view.text == ".")
+            {
+                if(canAddDecimal)
+                    workingsTV.append(view.text)
 
-    }
+                canAddDecimal = false
+            }
+            else
+                workingsTV.append(view.text)
 
-    private fun operateFunction() {
-        val listaOperadores :List<String> = tvInput.text.toString().split("(?<=[-+*/])|(?=[-+*/])")
-        for (listaOperadore in listaOperadores) {
-            
+            canAddOperation = true
         }
     }
 
-    private fun setProcess(value:String){
-        val progress = tvInput.text
-        tvInput.text = String.format("%s %s",progress,value)
+    fun operationAction(view: View)
+    {
+        if(view is Button && canAddOperation)
+        {
+            workingsTV.append(view.text)
+            canAddOperation = false
+            canAddDecimal = true
+        }
     }
 
-    private fun cleanCalculator(){
-        tvInput.text = ""
-        tvOutput.text = ""
+    fun allClearAction(view: View)
+    {
+        workingsTV.text = ""
+        resultsTV.text = ""
     }
 
+    fun backSpaceAction(view: View)
+    {
+        val length = workingsTV.length()
+        if(length > 0)
+            workingsTV.text = workingsTV.text.subSequence(0, length - 1)
+    }
+
+    fun equalsAction(view: View)
+    {
+        resultsTV.text = calculateResults()
+    }
+
+    private fun calculateResults(): String
+    {
+        val digitsOperators = digitsOperators()
+        if(digitsOperators.isEmpty()) return ""
+
+        val timesDivision = timesDivisionCalculate(digitsOperators)
+        if(timesDivision.isEmpty()) return ""
+
+        val result = addSubtractCalculate(timesDivision)
+        return result.toString()
+    }
+
+    private fun addSubtractCalculate(passedList: MutableList<Any>): Float
+    {
+        var result = passedList[0] as Float
+
+        for(i in passedList.indices)
+        {
+            if(passedList[i] is Char && i != passedList.lastIndex)
+            {
+                val operator = passedList[i]
+                val nextDigit = passedList[i + 1] as Float
+                if (operator == '+')
+                    result += nextDigit
+                if (operator == '-')
+                    result -= nextDigit
+            }
+        }
+
+        return result
+    }
+
+    private fun timesDivisionCalculate(passedList: MutableList<Any>): MutableList<Any>
+    {
+        var list = passedList
+        while (list.contains('x') || list.contains('/'))
+        {
+            list = calcTimesDiv(list)
+        }
+        return list
+    }
+
+    private fun calcTimesDiv(passedList: MutableList<Any>): MutableList<Any>
+    {
+        val newList = mutableListOf<Any>()
+        var restartIndex = passedList.size
+
+        for(i in passedList.indices)
+        {
+            if(passedList[i] is Char && i != passedList.lastIndex && i < restartIndex)
+            {
+                val operator = passedList[i]
+                val prevDigit = passedList[i - 1] as Float
+                val nextDigit = passedList[i + 1] as Float
+                when(operator)
+                {
+                    'x' ->
+                    {
+                        newList.add(prevDigit * nextDigit)
+                        restartIndex = i + 1
+                    }
+                    '/' ->
+                    {
+                        newList.add(prevDigit / nextDigit)
+                        restartIndex = i + 1
+                    }
+                    else ->
+                    {
+                        newList.add(prevDigit)
+                        newList.add(operator)
+                    }
+                }
+            }
+
+            if(i > restartIndex)
+                newList.add(passedList[i])
+        }
+
+        return newList
+    }
+
+    private fun digitsOperators(): MutableList<Any>
+    {
+        val list = mutableListOf<Any>()
+        var currentDigit = ""
+        for(character in workingsTV.text)
+        {
+            if(character.isDigit() || character == '.')
+                currentDigit += character
+            else
+            {
+                list.add(currentDigit.toFloat())
+                currentDigit = ""
+                list.add(character)
+            }
+        }
+
+        if(currentDigit != "")
+            list.add(currentDigit.toFloat())
+
+        return list
+    }
 
 }
